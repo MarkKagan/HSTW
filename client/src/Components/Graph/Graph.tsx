2596e6498b99368914199add804a59d8bbedc35b
Abalfazl Zareei <abalfazl.zareei@gmail.com>
2023-01-23 16:23:34 +0100

    Abi ts

--- client/src/Components/Graph/Graph.tsx
+++ client/src/Components/Graph/Graph.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Graph.css'
 import { useEffect, useState } from "react";
 import 'chart.js/auto';
@@ -10,13 +11,13 @@ export default function Graph({clicked, mobile}) {
   useEffect(() => {
     getCountrySpecificPastData(clicked['Alpha-2'], 4, setData)
   }, [clicked]);
-  
+
 
   return (
     <div id='graph-container'>
       <h3 id='graph-header'>Data Analysis of the previous week</h3>
       {
-          data 
+          data
           ? <Line data={data} width={mobile ? 250 : 800} height={mobile ? 200 : 800} />
           : <h3 id='graph-header' style={{'padding' : '150px 0 150px 0'}}>No data for this country</h3>
       }
